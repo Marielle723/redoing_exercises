75d93d52fe6c0e11e24a48489f8ec2b4bde59349
Marielle723 <https://github.com/Marielle723>
2022-05-05 18:50:24 +0200

    Configuring car selection

--- src/Components/Tesla Config/TeslaConfig.js
+++ src/Components/Tesla Config/TeslaConfig.js
@@ -1,17 +1,18 @@
 import React from "react";
 import TeslaTitle from "./TeslaTitle";
-import CarSelection from "./CarSelection";
-import ColorSelection from "./ColorSelection";
+
 import TeslaSubtitle from "./TeslaSubtitle";
 
 function TeslaConfig() {
   const title = "Tesla Config";
   const subtitles = ["Sélectionnez votre véhicule", "Sélectionnez la couleur"];
+  const carSelection = 
 
   return (
     <div>
       <TeslaTitle title={title} />
       <TeslaSubtitle subtitles={subtitles} />
+     
     </div>
   );
 }
--- src/Components/Tesla Config/TeslaSubtitle.js
+++ src/Components/Tesla Config/TeslaSubtitle.js
@@ -1,10 +1,30 @@
 import React from "react";
+import CarSelection from "./CarSelection";
+import ColorSelection from "./ColorSelection";
 
 function TeslaSubtitle({ subtitles }) {
+  const carSelection = [
+    { "Grande Autonomie": "90700" },
+    { " Performanc": "106700" },
+  ];
+
+  const colorSelection = [
+    { "Blanc Nacré Multicouches": "0" },
+    { "Noir Uni": "1000" },
+  ];
+
   return (
     <ul>
       {subtitles.map((subtitle, index) => {
-        return <li key={index}>{subtitle}</li>;
+        return (
+          <li key={index}>
+            <CarSelection
+              subtitle={subtitle}
+              carSelection={carSelection}
+              colorSelection={colorSelection}
+            />
+          </li>
+        );
       })}
     </ul>
   );
