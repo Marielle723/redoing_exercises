d805d6092f6fc58d301f348648047b9ffcf1273a
Marielle723 <https://github.com/Marielle723>
2022-06-19 13:54:58 +0200

    improvement in emoji search

--- src/Components/Emoji Search/Search.js
+++ src/Components/Emoji Search/Search.js
@@ -3,27 +3,36 @@ import React, { useState } from "react";
 function Search({ title, emojiList, setStartSearch, lineTab, setLineTab }) {
   const [search, setSearch] = useState("");
 
-  console.log(lineTab);
-
   const handleSearch = (e) => {
     e.preventDefault();
 
     const letters = e.target.value;
     setSearch(letters);
+    const newLineTab = [...lineTab];
     if (letters.length > 2) {
       setStartSearch(true);
       for (let i = 0; i < emojiList.length; i++) {
         const keywordsTab = emojiList[i].keywords.split(" ");
-        const goodWord = keywordsTab.filter(
-          (keyword) => keyword.indexOf(letters) !== -1
-        );
+        // const goodWord = keywordsTab.filter(
+        //   (keyword) => keyword.indexOf(letters) !== -1
+        // );
+
+        const goodWord = keywordsTab.filter((keyword) => keyword === letters);
+
         if (goodWord.length > 0) {
-          const newLineTab = [...lineTab];
           newLineTab.push(emojiList[i]);
+
           setLineTab(newLineTab);
+
+          goodWord.length = 0;
         }
       }
-    } else setStartSearch(false);
+    } else {
+      const newLineTab = [...lineTab];
+      newLineTab.length = 0;
+      setLineTab(newLineTab);
+      setStartSearch(false);
+    }
   };
 
   return (
